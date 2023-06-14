ba9d5426c18e22d619703d61dfe856ee4af794f1
rmo02 <ramoon.maia23@gmail.com>
2023-06-14 16:23:57 -0300

    criando branch mirante

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,8 @@ import { defalutTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
 import { Home } from "./pages/Home";
 
-export function App() {  
+export function App() {
+  // mhsdkasbc
   return (
    <ThemeProvider theme={defalutTheme}>
     <GlobalStyle />
